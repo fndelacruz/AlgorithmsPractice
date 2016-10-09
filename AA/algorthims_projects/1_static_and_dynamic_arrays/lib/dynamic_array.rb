dc2a7df1a681b7a94a2c7d5ff39f1ed16431804b
Florante Dela Cruz <fndelacruz@gmail.com>
2016-10-08 19:11:20 -0700

    AA algorithm exercises. add suboptimal dynamic array and ring buffer dynamic array

--- AA/algorthims_projects/1_static_and_dynamic_arrays/lib/dynamic_array.rb
+++ AA/algorthims_projects/1_static_and_dynamic_arrays/lib/dynamic_array.rb
@@ -3,32 +3,56 @@ require_relative "static_array"
 class DynamicArray
   attr_reader :length
 
-  def initialize
+  def initialize(size=8)
+    @store = StaticArray.new(size)
+    @length = 0
+    @capacity = size
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    check_index(index)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    check_index(0)
+    popped = @store[@length -= 1]
+    @store[@length] = nil
+    popped
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    @length += 1
+    resize! if @length > @capacity
+    @store[@length - 1] = val
+    @store
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    check_index(0)
+    shifted = @store[0]
+    (1...@length).each { |idx| @store[idx - 1] = @store[idx] }
+    @length -= 1
+    shifted
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    @length += 1
+    resize! if @length > @capacity
+    (@length - 1).downto(0).each { |idx| @store[idx + 1] = @store[idx] }
+    @store[0] = val
   end
 
   protected
@@ -36,9 +60,14 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
+    raise "index out of bounds" unless (0...@length).include?(index)
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    new_store = StaticArray.new(@capacity * 2)
+    (0...@capacity).each { |idx| new_store[idx] = @store[idx] }
+    @capacity *= 2
+    @store = new_store
   end
 end
--- AA/algorthims_projects/1_static_and_dynamic_arrays/lib/ring_buffer.rb
+++ AA/algorthims_projects/1_static_and_dynamic_arrays/lib/ring_buffer.rb
@@ -1,33 +1,62 @@
 require_relative "static_array"
-
+require "byebug"
 class RingBuffer
   attr_reader :length
 
-  def initialize
+  def initialize(size=8)
+    @store = StaticArray.new(8)
+    @length = 0
+    @capacity = size
+    @start_idx = 0
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    adjusted_idx = (@start_idx + index) % @capacity
+    @store[adjusted_idx]
   end
 
   # O(1)
   def []=(index, val)
+    check_index(index)
+    adjusted_idx = (@start_idx + index) % @capacity
+    @store[adjusted_idx] = val
   end
 
   # O(1)
   def pop
+    check_index(0)
+    last_idx = (length - 1 + @start_idx) % @capacity
+    popped = @store[last_idx]
+    @store[last_idx] = nil
+    @length -= 1
+    popped
   end
 
   # O(1) ammortized
   def push(val)
+    resize! if @length == @capacity
+    @length += 1
+    self[@length - 1] = val
   end
 
   # O(1)
   def shift
+    check_index(0)
+    shifted = @store[@start_idx]
+    @length -= 1
+    @store[@start_idx] = nil
+    @start_idx = (@start_idx + 1) % @capacity
+    shifted
   end
 
   # O(1) ammortized
   def unshift(val)
+    resize! if @length == @capacity
+    @length += 1
+    @start_idx = (@start_idx - 1) % @capacity
+    @store[@start_idx] = val
   end
 
   protected
@@ -35,8 +64,14 @@ class RingBuffer
   attr_writer :length
 
   def check_index(index)
+    raise "index out of bounds" unless (0...@length).include?(index)
   end
 
   def resize!
+    new_store = StaticArray.new(@capacity * 2)
+    (0...@length).each { |idx| new_store[idx] = self[idx] }
+    @capacity *= 2
+    @store = new_store
+    @start_idx = 0
   end
 end
