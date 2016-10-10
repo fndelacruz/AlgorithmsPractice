d524cbd38591318c7594f3b2f3b04d5ad2f36d13
Florante Dela Cruz <fndelacruz@gmail.com>
2016-10-10 13:20:50 -0700

    AA algorithm exercises. finish exercise 3: heap, heapsort

--- AA/algorthims_projects/3_heap_heapsort/lib/heap.rb
+++ AA/algorthims_projects/3_heap_heapsort/lib/heap.rb
@@ -1,32 +1,111 @@
+require "byebug"
 class BinaryMinHeap
+  DEFAULT_PROC = Proc.new { |el1, el2| el1 <=> el2 }
+
+  def self.child_indices(len, parent_index)
+    [2 * parent_index + 1, 2 * parent_index + 2].select { |idx| idx < len }
+  end
+
+  def self.parent_index(child_index)
+    raise "root has no parent" if child_index.zero?
+    (child_index - 1) / 2
+  end
+
+  def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    comparator = prc || DEFAULT_PROC
+    idx = parent_idx
+    while idx <= parent_index(len - 1)
+      current_el = array[idx]
+
+      # get idx of smallest (maximum) two children
+      child_indices = child_indices(len, idx)
+
+      # find child index of smaller element
+      if child_indices.size == 2
+        case comparator.call(*child_indices.map { |idx| array[idx] })
+        when -1
+          child_idx = child_indices.first
+        when 1
+          child_idx = child_indices.last
+        when 0
+          break
+        end
+      else
+        child_idx = child_indices.first
+      end
+
+      # stop heapifying down if current el is less than or equal to its smallest child
+      break unless comparator.call([current_el, array[child_idx]]) == 1
+
+      # swap current el with its smallest child
+      array[idx], array[child_idx] = array[child_idx], array[idx]
+
+      # move down the heap
+      idx = child_idx
+    end
+    array
+  end
+
+  def self.heapify_up(array, child_idx, len = array.length, &prc)
+    idx = child_idx
+    while idx > 0
+      parent_idx = parent_index(idx)
+
+      # stop heapifying up if current el is greater than its parent
+      break if array[idx] > array[parent_idx]
+
+      # swap current el with its parent
+      array[idx], array[parent_idx] = array[parent_idx], array[idx]
+
+      # move up the heap
+      idx = parent_idx
+    end
+    array
+  end
+
+  def self.heapify_up(array, child_idx, len = array.length, &prc)
+    proc = prc || DEFAULT_PROC
+    idx = child_idx
+    while idx > 0
+      parent_idx = parent_index(idx)
+
+      # stop heapifying up if current el is greater than its parent
+      break if proc.call(array[idx], array[parent_idx]) == 1
+
+      # swap current el with its parent
+      array[idx], array[parent_idx] = array[parent_idx], array[idx]
+
+      # move up the heap
+      idx = parent_idx
+    end
+    array
+  end
+
   def initialize(&prc)
+    @prc = prc
+    @store = []
   end
 
   def count
+    store.size
   end
 
   def extract
+    store[0], store[count - 1] = store[count - 1], store[0]
+    extracted = store.pop
+    self.class.heapify_down(store, 0, count, &prc)
+    extracted
   end
 
   def peek
+    store[0]
   end
 
   def push(val)
+    store.push(val)
+    self.class.heapify_up(store, count - 1, count, &prc)
   end
 
   protected
   attr_accessor :prc, :store
-
-  public
-  def self.child_indices(len, parent_index)
-  end
-
-  def self.parent_index(child_index)
-  end
-
-  def self.heapify_down(array, parent_idx, len = array.length, &prc)
-  end
-
-  def self.heapify_up(array, child_idx, len = array.length, &prc)
-  end
 end
--- AA/algorthims_projects/3_heap_heapsort/lib/heap_sort.rb
+++ AA/algorthims_projects/3_heap_heapsort/lib/heap_sort.rb
@@ -2,5 +2,25 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    descending_proc = Proc.new { |el1, el2| el2 <=> el1 }
+
+    # heapify array into a MAX HEAP for easy ASCENDING SORT
+    first_parent_node_idx = BinaryMinHeap.parent_index(size - 1)
+    first_parent_node_idx.downto(0).each do |idx|
+      BinaryMinHeap.heapify_down(self, idx, size, &descending_proc)
+    end
+
+    (size - 1).times do |idx|
+      # swap first unsorted element (next largest element) with last unsorted element.
+      self[0], self[-1 - idx] = self[-1 - idx], self[0]
+      # now, element at self[-1 - idx] is sorted
+
+      # do not heapify on last swap (array is now sorted!)
+      break if idx == size - 2
+
+      # heapify down the first unsorted element
+      BinaryMinHeap.heapify_down(self, 0, size - 1 - idx, &descending_proc)
+    end
+    self
   end
 end
